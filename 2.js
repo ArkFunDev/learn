36b913b79e5e3622a576ac252e119616875c0343
helmy ashari <helmyashari13@gmail>
2026-06-08 00:25:11 +0700

    delete 1,2,3,4,a.js

--- 2.js
+++
@@ -1,5 +0,0 @@
-for (let count = 1; count <= 3; count++) {
-    console.log("🌙 Malam ke-" + count + ": aku coba tidur... tapi yang ke-loop malah kamu.");
-}
-
-console.log("❤️ Fix, ini bukan infinite loop—ini perasaan.");
--- 3.js
+++
@@ -1,5 +0,0 @@
-let age = null; 
-if (age === null) {
-  console.log("nilai null");
-}
-console.log(typeof age);
